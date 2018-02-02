b15fde9a18ec82d55b315312635fc32c3f25e454
Nagendra <javahunk100@gmail.com>
2018-02-02 22:04:18 +0530

    updated comment.

--- src/main/java/com/spring/boot/ApplicationBootMain.java
+++ src/main/java/com/spring/boot/ApplicationBootMain.java
@@ -3,6 +3,11 @@ package com.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Nagendra
+ *   starting point of the program!
+ */
 @SpringBootApplication
 public class ApplicationBootMain {
 	
